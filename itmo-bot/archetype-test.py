c4a1c65774b7864b56682860d4d419865ce42328
billiamdan <timur.shagiakhmetov@gmail.com>
2019-11-01 12:04:00 +0300

    trying to understand how to make questionary in proper way

---
+++ itmo-bot/archetype-test.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Simple inline keyboard bot with multiple CallbackQueryHandlers.
+
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined as callback query handler. Then, those functions are
+passed to the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot that uses inline keyboard that has multiple CallbackQueryHandlers arranged in a
+ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line to stop the bot.
+"""
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, bot
+from telegram import ParseMode
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
+import logging
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+# Stages
+ANSWER, START, NEXT, FINISH = range(4)
+# Callback data
+ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN = range(7)
+
+
+def button(update, context):
+    query = update.callback_query
+
+    query.edit_message_text(text="Selected option: {}".format(query.data))
+
+
+def help(update, context):
+    update.message.reply_text("Use /start to test this bot.")
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def start_qestionary(update, context):
+    """Send message on `/start`."""
+    # Get user that sent /start and log his name
+    question = 1
+    user = update.message.from_user
+    chat_id = update.message.chat_id
+    logger.info("User %s started the conversation.", user.first_name)
+    # Build InlineKeyboard where each button has a displayed text
+    # and a string as callback_data
+    # The keyboard is a list of button rows, where each row is in turn
+    # a list (henc `[[...]]`).
+    keyboard = [
+        [InlineKeyboardButton(" 3 ", callback_data=str(ONE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(TWO)),
+         InlineKeyboardButton(" 1 ", callback_data=str(THREE)),
+         InlineKeyboardButton(" 0 ", callback_data=str(FOUR)),
+         InlineKeyboardButton(" 1 ", callback_data=str(FIVE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(SIX)),
+         InlineKeyboardButton(" 3 ", callback_data=str(SEVEN))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    # Send message with text and appended InlineKeyboard
+
+    update.message.reply_photo(photo=open('archetype-test/' + str(question) + '.png', 'rb'), reply_markup=reply_markup)
+    # Tell CosversationHandler that we're in State `FIRST` now
+    return ANSWER
+
+
+def next(update, context):
+    """Prompt same text & keyboard as `start` does but not as new message"""
+    # Get CallbackQuery from Update
+    # Get Bot from CallbackContext
+    print("@@@@@@@@@    Entered in NEXT     @@@@@@@")
+    keyboard = [
+        [InlineKeyboardButton(" 3 ", callback_data=str(ONE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(TWO)),
+         InlineKeyboardButton(" 1 ", callback_data=str(THREE)),
+         InlineKeyboardButton(" 0 ", callback_data=str(FOUR)),
+         InlineKeyboardButton(" 1 ", callback_data=str(FIVE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(SIX)),
+         InlineKeyboardButton(" 3 ", callback_data=str(SEVEN))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    question = file_len("archetype-test-answers/" + str(chat_id) + "-answers"".txt")
+    print("@@###@#@#@#@#@     Question is   " + str(question) + "       ")
+    # Instead of sending a new message, edit the message that
+    # originated the CallbackQuery. This gives the feeling of an
+    # interactive menu.
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        photo=open('archetype-test/' + str(question) + '.png', 'rb'),
+        reply_markup=reply_markup
+    )
+    print("@@@@@@@@@    PHOTO UPDATED HAHA     @@@@@@@")
+    # update.message.reply_photo(photo=open('archetype-test/' + str(question) + '.png', 'rb'), reply_markup=reply_markup)
+    return ANSWER
+
+
+def one(update, context):
+    """Show new choice of buttons"""
+    print("@@@@@@@@@    Entered in ONE     @@@@@@@")
+    keyboard = [
+        [InlineKeyboardButton(" 3 ", callback_data=str(ONE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(TWO)),
+         InlineKeyboardButton(" 1 ", callback_data=str(THREE)),
+         InlineKeyboardButton(" 0 ", callback_data=str(FOUR)),
+         InlineKeyboardButton(" 1 ", callback_data=str(FIVE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(SIX)),
+         InlineKeyboardButton(" 3 ", callback_data=str(SEVEN))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+    print("@@@@@@@@@    PREPARED     @@@@@@@")
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write("3;;;;;;;\r\n")
+    f.close()
+    print("@@@@@@@@@    writen     @@@@@@@")
+    update.message.reply_photo(photo=open('archetype-test/2.png', 'rb'), reply_markup=reply_markup)
+    photo = open('archetype-test/2.png', 'rb')
+    context.bot.edit_message_media(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        media=photo.fileno(),
+        reply_markup=reply_markup
+    )
+    print("@@@@@@@@@    EDITED     @@@@@@@")
+    return NEXT
+
+
+def two(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 2 ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";2;;;;;;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    return NEXT
+
+
+def three(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 1 ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";;1;;;;;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    # Transfer to conversation state `SECOND`
+    return NEXT
+
+
+def four(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 0 ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";;;0;;;;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    return NEXT
+
+
+def five(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 1 ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";;;;1;;;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    return NEXT
+
+
+def six(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 2 ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";;;;;2;;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    return NEXT
+
+
+def seven(update, context):
+    """Show new choice of buttons"""
+    keyboard = [
+        [InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton("   ", callback_data=str()),
+         InlineKeyboardButton(" 3 ", callback_data=str())]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    query = update.callback_query
+    chat_id = query.message.chat_id
+
+    f = open("archetype-test-answers/" + str(chat_id) + "-answers"".txt", "a+")
+    f.write(";;;;;;3;\r\n")
+    f.close()
+
+    context.bot.edit_message_text(
+        chat_id=chat_id,
+        message_id=query.message.message_id,
+        text="Записано",
+        reply_markup=reply_markup
+    )
+    return NEXT
+
+
+def end(update, context):
+    """Returns `ConversationHandler.END`, which tells the
+    ConversationHandler that the conversation is over"""
+    query = update.callback_query
+    context.bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text="Вы прошли опросник. Спасибо! Ваши ответы записаны и будут проанализированы."
+    )
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def gettoken(source):
+    with open(source, 'r') as file:
+        token = file.read().replace('\n', '')
+    return token
+
+def file_len(fname):
+    with open(fname) as f:
+        for i in enumerate(f):
+            pass
+    return i + 1
+
+def main():
+    # Create the Updater and pass it your bot's token.
+
+    updater = Updater(gettoken("tokens/getmethroughbot-token"), use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Setup conversation handler with the states FIRST and SECOND
+    # Use the `pattern parameter to pass CallbackQueryies with specific
+    # data pattern to the corresponding ha`ndlers.
+    # ^ means "start of line/string"
+    # $ means "end of line/string"
+    # So ^ABC$ will only allow 'ABC'
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start_qestionary)],
+        states={
+            ANSWER: [CallbackQueryHandler(one, pattern='^' + str(ONE) + '$'),
+                    CallbackQueryHandler(two, pattern='^' + str(TWO) + '$'),
+                    CallbackQueryHandler(three, pattern='^' + str(THREE) + '$'),
+                    CallbackQueryHandler(four, pattern='^' + str(FOUR) + '$'),
+                    CallbackQueryHandler(five, pattern='^' + str(FIVE) + '$'),
+                    CallbackQueryHandler(six, pattern='^' + str(SIX) + '$'),
+                    CallbackQueryHandler(seven, pattern='^' + str(SEVEN) + '$')],
+            NEXT:   [CallbackQueryHandler(next, pattern='^' + str() + '$')],
+            FINISH: [CallbackQueryHandler(end, pattern='^' + str() + '$')]
+        },
+        fallbacks=[CommandHandler('start', start_qestionary)]
+    )
+
+    # Add conversationhandler to dispatcher it will be used for handling
+    # updates
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
