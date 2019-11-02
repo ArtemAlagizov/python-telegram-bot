6d83e0c37fc16d2ba98fd70aa46d1c3d7c594eba
billiamdan <timur.shagiakhmetov@gmail.com>
2019-11-03 01:13:48 +0300

    GARBADGE MOVED

--- itmo-bot/facilitator-bot.py
+++ itmo-bot/facilitator-bot.py
@@ -16,8 +16,9 @@ bot.
 
 from datetime import datetime, date, time
 from telegram import ReplyKeyboardMarkup
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
-                          ConversationHandler, PicklePersistence)
+                          ConversationHandler, PicklePersistence, CallbackQueryHandler)
 
 import logging
 
@@ -27,26 +28,41 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-CHOOSING, TYPING_REPLY, TYPING_CHOICE = range(3)
+CHOOSING, TYPING_REPLY, TYPING_CHOICE, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN = range(10)
+
 
 intro_choice_1 = u'Изучить новую теорию'
 intro_choice_2 = u'Сформулировать конкретные шаги в развитии карьеры'
 intro_choice_3 = u'Понять как работает система'
 intro_choice_4 = u'Лучше узнать себя'
 
-intro_reply_1 = u'Отлично! За эти 10 дней ты познакомишься с 4х-частной моделью описания корпоративных культур и ' \
+intro_reply_button_1 = u'Отлично! За эти 10 дней ты познакомишься с 4х-частной моделью описания корпоративных культур и ' \
                 u'профессий. С ее помощью ты сможешь сформулировать стратегию следующих шагов в своей карьере '
-intro_reply_2 = u'Супер! За эти 10 дней ты получишь понятный и работающий инструмент для развития в карьере. Ты ' \
+intro_reply_button_2 = u'Супер! За эти 10 дней ты получишь понятный и работающий инструмент для развития в карьере. Ты ' \
                 u'поймешь, в чем твои сильные стороны и где сможешь добиться максимальных результатов! '
-intro_reply_3 = u'Прекрасно! За эти 10 дней ты узнаешь на примере 4частной модели, какие бывают корпоративные ' \
+intro_reply_button_3 = u'Прекрасно! За эти 10 дней ты узнаешь на примере 4частной модели, какие бывают корпоративные ' \
                 u'культуры и виды профессий, и как это сочетание влияет на развитие твоей карьеры '
-intro_reply_4 = u'Замечательно! За эти 10 дней ты лучше узнаешь, какие компании и корпоративные культуры тебе ' \
+intro_reply_button_4 = u'Замечательно! За эти 10 дней ты лучше узнаешь, какие компании и корпоративные культуры тебе ' \
                 u'подходят, а какие противопоказаны; в чем область твоих талантов, и как ты можешь их применить! '
+
+default_reply_button_1 = u'Пройти тест'
+default_reply_button_2 = u'xxx'
+default_reply_button_3 = u'Help FAQ'
+default_reply_button_4 = u'Задать вопрос автору'
+
 broadcast_1_reply = u'Начинается подкаст 1 ({}), не пропусти!'
 broadcast_2_reply = u'Начинается подкаст 2 ({}), не пропусти!'
 broadcast_3_reply = u'Начинается подкаст 3 ({}), не пропусти!'
 broadcast_4_reply = u'Начинается подкаст 4 ({}), не пропусти!'
 broadcast_5_reply = u'Начинается подкаст 5 ({}), не пропусти!'
+
+intro_questionary_reply_keyboard = [[intro_choice_1, intro_choice_2],
+                                    [intro_choice_3, intro_choice_4]]
+default_facilitator_keyboard = [[default_reply_button_1, default_reply_button_2],
+                                [default_reply_button_3, default_reply_button_4]]
+
+intro_markup = ReplyKeyboardMarkup(intro_questionary_reply_keyboard, one_time_keyboard=True)
+default_markup = ReplyKeyboardMarkup(default_facilitator_keyboard, one_time_keyboard=True)
 
 # job_due_1 = datetime.combine(date(2019, 11, 7), time(11, 00))
 # job_due_2 = datetime.combine(date(2019, 11, 7), time(19, 00))
@@ -94,150 +110,68 @@ job_due_18 = datetime.combine(date(2019, 11, 2), time(job_due_base_1, job_due_ba
 job_due_19 = datetime.combine(date(2019, 11, 2), time(job_due_base_1, job_due_base_2 + 16))
 job_due_20 = datetime.combine(date(2019, 11, 2), time(job_due_base_1, job_due_base_2 + 17))
 
-intro_questionary_reply_keyboard = [[intro_choice_1, intro_choice_2],
-                                    [intro_choice_3, intro_choice_4]]
-default_facilitator_keyboard = [["intro_choice_1", "intro_choice_2"],
-                                ["intro_choice_3", "intro_choice_4"]]
-
-intro_markup = ReplyKeyboardMarkup(intro_questionary_reply_keyboard, one_time_keyboard=True)
-intro_end_markup = ReplyKeyboardMarkup([], one_time_keyboard=True)
-
-
-def facts_to_str(user_data):
-    facts = list()
-
-    for key, value in user_data.items():
-        facts.append('{} - {}'.format(key, value))
-
-    return "\n".join(facts).join(['\n', '\n'])
-
 
 def intro_choice_1_callback(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(intro_reply_1, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(intro_reply_button_1, reply_markup=default_markup)
+    return CHOOSING
 
 
 def intro_choice_2_callback(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(intro_reply_2, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(intro_reply_button_2, reply_markup=default_markup)
+    return CHOOSING
 
 
 def intro_choice_3_callback(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(intro_reply_3, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(intro_reply_button_3, reply_markup=default_markup)
+    return CHOOSING
 
 
 def intro_choice_4_callback(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(intro_reply_4, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(intro_reply_button_4, reply_markup=default_markup)
+    return CHOOSING
 
 
 def broadcast_1(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(broadcast_1_reply, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(broadcast_1_reply, reply_markup=default_markup)
+    return CHOOSING
 
 
 def broadcast_2(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(broadcast_2_reply, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(broadcast_2_reply, reply_markup=default_markup)
+    return CHOOSING
 
 
 def broadcast_3(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(broadcast_3_reply, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(broadcast_3_reply, reply_markup=default_markup)
+    return CHOOSING
 
 
 def broadcast_4(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(broadcast_4_reply, reply_markup=intro_end_markup)
-    return ConversationHandler.END
+    update.message.reply_text(broadcast_4_reply, reply_markup=default_markup)
+    return CHOOSING
 
 
 def broadcast_5(update, context):
     if 'choice' in context.user_data:
         del context.user_data['choice']
-    update.message.reply_text(broadcast_5_reply, reply_markup=intro_end_markup)
-    return ConversationHandler.END
-
-
-def start(update, context):
-    reply_text = "Привет, рада тебя приветствовать на нашем марафоне! "
-    if context.user_data:
-        reply_text += " You already told me your {}. Why don't you tell me something more " \
-                      "about yourself? Or change enything I " \
-                      "already know.".format(", ".join(context.user_data.keys()))
-    else:
-        reply_text += " Расскажи, что тебя интересует в первую очередь"
-    update.message.reply_text(reply_text, reply_markup=intro_markup)
-
-    add_jobs(update, context)
-
+    update.message.reply_text(broadcast_5_reply, reply_markup=default_markup)
     return CHOOSING
-
-
-def regular_choice(update, context):
-    text = update.message.text.lower()
-    context.user_data['choice'] = text
-    if context.user_data.get(text):
-        reply_text = 'Your {}, I already know the following ' \
-                     'about that: {}'.format(text, context.user_data[text])
-    else:
-        reply_text = 'Your {}? Yes, I would love to hear about that!'.format(text)
-    update.message.reply_text(reply_text)
-
-    return TYPING_REPLY
-
-
-def custom_choice(update, context):
-    update.message.reply_text('Alright, please send me the category first, '
-                              'for example "Most impressive skill"')
-
-    return TYPING_CHOICE
-
-
-def received_information(update, context):
-    text = update.message.text
-    category = context.user_data['choice']
-    context.user_data[category] = text.lower()
-    del context.user_data['choice']
-
-    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
-                              "{}"
-                              "You can tell me more, or change your opinion on "
-                              "something.".format(facts_to_str(context.user_data)),
-                              reply_markup=intro_markup)
-
-    return CHOOSING
-
-
-def show_data(update, context):
-    update.message.reply_text("This is what you already told me:"
-                              "{}".format(facts_to_str(context.user_data)))
-
-
-def done(update, context):
-    if 'choice' in context.user_data:
-        del context.user_data['choice']
-
-    update.message.reply_text("I learned these facts about you:"
-                              "{}"
-                              "Until next time!".format(facts_to_str(context.user_data)))
-    return ConversationHandler.END
 
 
 def execute_job_1(context):
@@ -247,97 +181,110 @@ def execute_job_1(context):
 
 def execute_job_2(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 2 done!')
+    question = 1
+    context.bot.sendPhoto(job.context, photo=open('archetype-test/' + str(question) + '.png', 'rb'), reply_markup=default_markup)
 
 
 def execute_job_3(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 3 done!')
+    keyboard = [
+        [InlineKeyboardButton(" 3 ", callback_data=str(ONE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(TWO)),
+         InlineKeyboardButton(" 1 ", callback_data=str(THREE)),
+         InlineKeyboardButton(" 0 ", callback_data=str(FOUR)),
+         InlineKeyboardButton(" 1 ", callback_data=str(FIVE)),
+         InlineKeyboardButton(" 2 ", callback_data=str(SIX)),
+         InlineKeyboardButton(" 3 ", callback_data=str(SEVEN))]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    context.bot.sendMessage(job.context, text="Start handler, Choose a routes, Choose a routes, Choose a routes, "
+                                              "Choose a routes\nStart handler, Choose a routes, Choose a routes",
+                            parse_mode='Markdown', reply_markup=reply_markup)
 
 
 def execute_job_4(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 4 done!')
+    #context.bot.send_message(job.context, text='job 4 done!')
 
 
 def execute_job_5(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 5 done!')
+    #context.bot.send_message(job.context, text='job 5 done!')
 
 
 def execute_job_6(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 6 done!')
+    #context.bot.send_message(job.context, text='job 6 done!')
 
 
 def execute_job_7(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 7 done!')
+    #context.bot.send_message(job.context, text='job 7 done!')
 
 
 def execute_job_8(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 8 done!')
+    #context.bot.send_message(job.context, text='job 8 done!')
 
 
 def execute_job_9(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 9 done!')
+    #context.bot.send_message(job.context, text='job 9 done!')
 
 
 def execute_job_10(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 10 done!')
+    #context.bot.send_message(job.context, text='job 10 done!')
 
 
 def execute_job_11(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 11 done!')
+    #context.bot.send_message(job.context, text='job 11 done!')
 
 
 def execute_job_12(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 12 done!')
+    #context.bot.send_message(job.context, text='job 12 done!')
 
 
 def execute_job_13(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 13 done!')
+    #context.bot.send_message(job.context, text='job 13 done!')
 
 
 def execute_job_14(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 14 done!')
+    #context.bot.send_message(job.context, text='job 14 done!')
 
 
 def execute_job_15(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 15 done!')
+    #context.bot.send_message(job.context, text='job 15 done!')
 
 
 def execute_job_16(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 16 done!')
+    #context.bot.send_message(job.context, text='job 16 done!')
 
 
 def execute_job_17(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 17 done!')
+    #context.bot.send_message(job.context, text='job 17 done!')
 
 
 def execute_job_18(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 18 done!')
+    #context.bot.send_message(job.context, text='job 18 done!')
 
 
 def execute_job_19(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 19 done!')
+    #context.bot.send_message(job.context, text='job 19 done!')
 
 
 def execute_job_20(context):
     job = context.job
-    context.bot.send_message(job.context, text='job 20 done!')
+    #context.bot.send_message(job.context, text='job 20 done!')
 
 
 def add_jobs(update, context):
@@ -406,10 +353,10 @@ def add_jobs(update, context):
         new_job_20 = context.job_queue.run_once(execute_job_20, job_due_20, context=chat_id)
         context.chat_data['job'] = new_job_20
 
-        update.message.reply_text('Job is set in the queue...')
+        update.message.reply_text('attempt: jobs to be sent in the queue...')
 
     except (IndexError, ValueError):
-        update.message.reply_text('something gone wrong while setting jobs On')
+        update.message.reply_text('attempt:  something gone wrong while setting jobs On')
 
 
 def remove_job(update, context):
@@ -425,6 +372,82 @@ def remove_job(update, context):
     update.message.reply_text('job removed')
 
 
+def facts_to_str(user_data):
+    facts = list()
+
+    for key, value in user_data.items():
+        facts.append('{} - {}'.format(key, value))
+
+    return "\n".join(facts).join(['\n', '\n'])
+
+
+def start(update, context):
+    reply_text = "Привет, рада тебя приветствовать на нашем марафоне! "
+    if context.user_data:
+        reply_text += " You already told me your {}. Why don't you tell me something more " \
+                      "about yourself? Or change enything I " \
+                      "already know.".format(", ".join(context.user_data.keys()))
+    else:
+        reply_text += " Расскажи, что тебя интересует в первую очередь"
+    update.message.reply_text(reply_text, reply_markup=intro_markup)
+
+    add_jobs(update, context)
+
+    return CHOOSING
+
+
+def default_choice(update, context):
+    text = update.message.text.lower()
+    context.user_data['choice'] = text
+    if context.user_data.get(text):
+        reply_text = 'Your {}, I already know the following ' \
+                     'about that: {}'.format(text, context.user_data[text])
+    else:
+        reply_text = 'Your {}? Yes, I would love to hear about that!'.format(text)
+    update.message.reply_text(reply_text)
+
+    return TYPING_REPLY
+
+
+def custom_choice(update, context):
+    update.message.reply_text('Alright, please send me the category first, '
+                              'for example "Most impressive skill"')
+
+    return TYPING_CHOICE
+
+
+def received_information(update, context):
+    text = update.message.text
+    category = context.user_data['choice']
+    context.user_data[category] = text.lower()
+    del context.user_data['choice']
+
+    update.message.reply_text("Neat! Just so you know, this is what you already told me:"
+                              "{}"
+                              "You can tell me more, or change your opinion on "
+                              "something.".format(facts_to_str(context.user_data)),
+                              reply_markup=default_markup)
+
+    return CHOOSING
+
+
+def show_data(update, context):
+    update.message.reply_text("This is what you already told me:"
+                              "{}".format(facts_to_str(context.user_data)))
+
+
+def done(update, context):
+    if 'choice' in context.user_data:
+        del context.user_data['choice']
+
+    update.message.reply_text("I learned these facts about you:"
+                              "{}"
+                              "Until next time!".format(facts_to_str(context.user_data)))
+    return ConversationHandler.END
+
+
+
+
 def error(update, context):
     """Log Errors caused by Updates."""
     logger.warning('Update "%s" caused error "%s"', update, context.error)
@@ -434,7 +457,8 @@ def gettoken(source):
     with open(source, 'r') as file:
         token = file.read().replace('\n', '')
     return token
-
+default_facilitator_keyboard = [[u'Пройти тест', u''],
+                                [u'Help FAQ', u'Вопрос автору']]
 
 def main():
     # Create the Updater and pass it your bot's token.
@@ -449,8 +473,14 @@ def main():
         entry_points=[CommandHandler('start', start)],
 
         states={
-            CHOOSING: [MessageHandler(Filters.regex('^(Age|Favour|Number)$'),
-                                      regular_choice),
+            CHOOSING: [MessageHandler(Filters.regex(default_reply_button_1),
+                                      default_choice),
+                       MessageHandler(Filters.regex(default_reply_button_2),
+                                      default_choice),
+                       MessageHandler(Filters.regex(default_reply_button_3),
+                                      default_choice),
+                       MessageHandler(Filters.regex(default_reply_button_4),
+                                      default_choice),
                        MessageHandler(Filters.regex('^Something else...$'),
                                       custom_choice),
                        MessageHandler(Filters.regex(intro_choice_1),
@@ -464,7 +494,7 @@ def main():
                        ],
 
             TYPING_CHOICE: [MessageHandler(Filters.text,
-                                           regular_choice),
+                                           default_choice),
                             ],
 
             TYPING_REPLY: [MessageHandler(Filters.text,
